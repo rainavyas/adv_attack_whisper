6fd1239c3a343f1964acd9194cbca5686d37893c
Rao Ma <marao2048@gmail.com>
2024-01-27 09:20:21 +0000

    Add support for Whisper

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,3 +1,4 @@
+LIBRISPEECH_DIR = '/home/rm2114/rds/rds-altaslp-8YSp2LXTlkY/data/librispeech'
 
 
 def load_data(core_args):
@@ -13,6 +14,19 @@ def load_data(core_args):
         return _librispeech()
 
 
-def _librispeech():
-    pass
-    #TODO
+def _librispeech(sub_dir):
+    '''
+        for clean audio, set `sub_dir' to dev_clean/test_clean as dev/test sets
+        for noisy audio, set `sub_dir' to dev_other/test_other as dev/test sets
+    '''
+    audio_transcript_pair_list = []
+    with open(f'{LIBRISPEECH_DIR}/{sub_dir}/audio_ref_pair_list') as fin:
+        for line in fin:
+            _, audio, ref = line.split(None, 2)
+            sample = {
+                    'audio': audio,
+                    'ref': ref
+                }
+            audio_transcript_pair_list.append(sample)
+                
+    return audio_transcript_pair_list
--- src/models/whisper.py
+++ src/models/whisper.py
@@ -1,14 +1,37 @@
 import torch
+import whisper
+import editdistance
+
+from whisper.normalizers import EnglishTextNormalizer
+
+CACHE_DIR = '~/rds/rds-altaslp-8YSp2LXTlkY/experiments/rm2114/.cache'
 
 class WhisperModel:
     '''
         Wrapper for Whisper ASR Transcription
     '''
-    def __init__(self, model_name, device=torch.device('cpu')):
-        self.model = #TODO
+    def __init__(self, model_name='small.en', device=torch.device('cpu')):
+        self.model = whisper.load_model(model_name, device=device, download_root=CACHE_DIR)
+        self.std = EnglishTextNormalizer()
     
     def predict(self, audio='', decoder_text=''):
         '''
             Whisper decoder output here
         '''
-        pass
+        result = self.model.transcribe(audio, language='en', initial_prompt=decoder_text)
+        segments = []
+        for segment in result['segments']:
+            segments.append(segment['text'].strip())
+            print(segment['start'], segment['end'], segment['text'])
+        return ' '.join(segments)
+
+    def calc_wer(self, hyp_list, ref_list):
+        '''
+            Calculate WER on the entire set, apply standard text normalisation before calculation
+        '''
+        errors, refs = 0, 0
+        for hyp, ref in hyp_list, ref_list:
+            hyp_tn, ref_tn = self.std(hyp), self.std(ref)
+            errors += editdistance.eval(hyp_tn.split(), ref_tn.split())
+            refs += len(ref_tn.split())
+        return errors / refs
